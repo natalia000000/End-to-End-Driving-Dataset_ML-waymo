a45477c66301e13b9988eb9508859d04f004cc57
natalia000000 <nataliaparedesbustamante@gmail.com>
2026-08-28 21:58:29 -0400

    Waymo

---
+++ src/waymo/pipelines/data_processing/nodes.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from pyspark.sql import DataFrame as SparkDataFrame
+
+
+def _is_true(x: pd.Series) -> pd.Series:
+    return x == "t"
+
+
+def _parse_percentage(x: pd.Series) -> pd.Series:
+    x = x.str.replace("%", "")
+    x = x.astype(float) / 100
+    return x
+
+
+def _parse_money(x: pd.Series) -> pd.Series:
+    x = x.str.replace("$", "").str.replace(",", "")
+    x = x.astype(float)
+    return x
+
+
+def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
+    """Preprocesses the data for companies.
+
+    Args:
+        companies: Raw data.
+    Returns:
+        Preprocessed data, with `company_rating` converted to a float and
+        `iata_approved` converted to boolean.
+    """
+    companies["iata_approved"] = _is_true(companies["iata_approved"])
+    companies["company_rating"] = _parse_percentage(companies["company_rating"])
+    return companies
+
+
+def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
+    """Preprocesses the data for shuttles.
+
+    Args:
+        shuttles: Raw data.
+    Returns:
+        Preprocessed data, with `price` converted to a float and `d_check_complete`,
+        `moon_clearance_complete` converted to boolean.
+    """
+    shuttles["d_check_complete"] = _is_true(shuttles["d_check_complete"])
+    shuttles["moon_clearance_complete"] = _is_true(shuttles["moon_clearance_complete"])
+    shuttles["price"] = _parse_money(shuttles["price"])
+    return shuttles
+
+
+def preprocess_reviews(reviews: pd.DataFrame) -> pd.DataFrame:
+    # Drop columns that aren't used for model training
+    cols_to_drop = [
+        'review_scores_comfort',
+        'review_scores_amenities',
+        'review_scores_trip',
+        'review_scores_crew',
+        'review_scores_location',
+        'review_scores_price',
+        'number_of_reviews',
+        'reviews_per_month',
+    ]
+    return reviews.drop(columns=cols_to_drop, errors="ignore")
+
+
+def create_model_input_table(
+    shuttles: SparkDataFrame, companies: SparkDataFrame, reviews: SparkDataFrame
+) -> SparkDataFrame:
+    """Combines all data to create a model input table.
+
+    Args:
+        shuttles: Preprocessed data for shuttles.
+        companies: Preprocessed data for companies.
+        reviews: Raw data for reviews.
+    Returns:
+        Model input table.
+
+    """
+    # Rename columns to prevent duplicates
+    shuttles = shuttles.withColumnRenamed("id", "shuttle_id")
+    companies = companies.withColumnRenamed("id", "company_id")
+
+    rated_shuttles = shuttles.join(reviews, "shuttle_id", how="left")
+    model_input_table = rated_shuttles.join(companies, "company_id", how="left")
+    model_input_table = model_input_table.dropna()
+    return model_input_table
---
+++ src/waymo/pipelines/data_processing/pipeline.py
@@ -0,0 +1,39 @@
+from kedro.pipeline import Node, Pipeline
+
+from .nodes import (
+    create_model_input_table,
+    preprocess_companies,
+    preprocess_reviews,
+    preprocess_shuttles,
+)
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline(
+        [
+            Node(
+                func=preprocess_companies,
+                inputs="companies",
+                outputs="preprocessed_companies",
+                name="preprocess_companies_node",
+            ),
+            Node(
+                func=preprocess_shuttles,
+                inputs="shuttles",
+                outputs="preprocessed_shuttles",
+                name="preprocess_shuttles_node",
+            ),
+            Node(
+                func=preprocess_reviews,
+                inputs="reviews",
+                outputs="preprocessed_reviews",
+                name="preprocess_reviews_node",
+            ),
+            Node(
+                func=create_model_input_table,
+                inputs=["preprocessed_shuttles", "preprocessed_companies", "preprocessed_reviews"],
+                outputs="model_input_table@spark",
+                name="create_model_input_table_node",
+            ),
+        ]
+    )
